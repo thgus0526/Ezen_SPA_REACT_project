09d09b0c5d70cdbf849b8d53e38b9ef524765712
Sungjin <byun970@gmail.com>
2024-06-04 16:49:00 +0900

    테스트 삭제

--- test5.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const test = () => {
-  return <div>옥지얌
-  </div>;
-};
-
-export default test;
--- test6.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const test = () => {
-  return <div>탕탕후루후루
-  </div>;
-};
-
-export default test;
